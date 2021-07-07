7eb17f01bd79049524bb27fdf64c8b847f203a2a
unknown <jinzagon@gmail.com>
2021-07-07 12:14:40 +0100

    all

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,112 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <!--<div class="items-center h-8" style="background-color:#cbe3e6">
+            <div class="items-center justify-between w-full px-5 overflow-y-auto tflex whitespace-nowrap scroll-hidden ">
+              <div class="flex flex-col flex-wrap mx-auto md:items-center md:flex-row">
+                <a href="/" class="pr-2 lg:pr-8 lg:px-6 focus:outline-none">
+                  <div class="inline-flex items-center">
+                    <div class="w-2 h-2 p-2 mr-2 rounded-full bg-gradient-to-tr from-blue-500 to-blue-600">
+                    </div>
+                    <h2 class="block p-2 text-xl font-medium tracking-tighter text-black transition duration-500 ease-in-out transform cursor-pointer hover:text-blueGray-500 lg:text-x lg:mr-8"> wickedblocks </h2>
+                  </div>
+                </a>
+                <nav class="flex flex-wrap items-center justify-start text-base ">
+                  <ul class="items-center inline-block list-none lg:inline-flex">
+                    <li>
+                      <a href="#" class="px-4 py-1 mr-1 text-base text-blueGray-500 transition duration-500 ease-in-out transform rounded-md focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2 hover:text-black ">About</a>
+                    </li>
+                    <li>
+                      <a href="#" class="px-4 py-1 mr-1 text-base text-blueGray-500 transition duration-500 ease-in-out transform rounded-md focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2 hover:text-black ">Partner Program</a>
+                    </li>
+                    <li>
+                      <a href="#" class="px-4 py-1 mr-1 text-base text-blueGray-500 transition duration-500 ease-in-out transform rounded-md focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2 hover:text-black ">Franchise</a>
+                    </li>
+                    <li>
+                      <a href="#" class="px-4 py-1 mr-1 text-base text-blueGray-500 transition duration-500 ease-in-out transform rounded-md focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2 hover:text-black ">How to Order</a>
+                    </li>
+                     <li>
+                      <a href="#" class="px-4 py-1 mr-1 text-base text-blueGray-500 transition duration-500 ease-in-out transform rounded-md focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2 hover:text-black ">Contact</a>
+                    </li>
+                  </ul>
+                </nav>
+                <button class="w-auto px-8 py-2 my-2 ml-auto text-base font-medium text-white transition duration-500 ease-in-out transform bg-blue-600 border-blue-600 rounded-md focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2 hover:b-gblue-700">Button </button>
+              </div>
+            </div>
+          </div>-->
+
+    <menu-component></menu-component>
+
+    <section class="text-blueGray-700 ">
+            <div class="container items-center px-5 py-8 mx-auto lg:px-16">
+              <div class="flex flex-wrap mb-12 text-left">
+                <div class="w-full p-1 mx-auto lg:w-1/4">
+                  <div class="p-4 lg:p-2 rounded-xl bg-blueGray-50">
+                    <img
+          src="https://spacejob.co.jp/wp-content/uploads/2021/03/revolt-164_6wVEHfI-unsplash-1024x683.jpg"
+          class=" rounded-btn object-cover xl:h-60 xl:w-72 object-center w-full lg:h-48 md:h-36"
+        />
+                    <!--<img class="object-cover object-center w-full mb-8 lg:h-48 md:h-36 rounded-xl" src="https://dummyimage.com/720x400/F3F4F7/8693ac" alt="blog">-->
+ <div class="content flex mt-4"><p class="w-8/12"> Vestibulum tincidunt tellus a metus hendrer </p><p class=" w-3/6 items-start text-right font-bold text-lg" style="direction: rtl;"> 199.23 $ </p></div>
+                    <a href="#" class="inline-flex items-center mt-4 font-semibold text-blue-600 lg:mb-0 hover:text-black " title="read more"> Read More » </a>
+                  </div>
+                </div>
+                <div class="w-full p-1 mx-auto lg:w-1/4">
+                  <div class="p-4 lg:p-2 rounded-xl bg-blueGray-50">
+                    <img class="object-cover object-center w-full mb-8 lg:h-48 md:h-36 rounded-xl" src="https://dummyimage.com/720x400/F3F4F7/8693ac" alt="blog">
+                    <h1 class="mx-auto mb-8 text-2xl font-semibold leading-none tracking-tighter text-black title-font"> Short length headline. </h1>
+                    <p class="mx-auto text-base font-medium leading-relaxed text-blueGray-700 ">You're about to launch soon and must be 100% focused on your product. Don't loose precious days designing, coding the landing page and testing . </p>
+                    <a href="#" class="inline-flex items-center mt-4 font-semibold text-blue-600 lg:mb-0 hover:text-black " title="read more"> Read More » </a>
+                  </div>
+                </div>
+                <div class="w-full p-1 mx-auto lg:w-1/4">
+                  <div class="p-4 lg:p-2 rounded-xl bg-blueGray-50">
+                    <img class="object-cover object-center w-full mb-8 lg:h-48 md:h-36 rounded-xl" src="https://dummyimage.com/720x400/F3F4F7/8693ac" alt="blog">
+                    <h1 class="mx-auto mb-8 text-2xl font-semibold leading-none tracking-tighter text-black title-font"> Short length headline. </h1>
+                    <p class="mx-auto text-base font-medium leading-relaxed text-blueGray-700 ">You're about to launch soon and must be 100% focused on your product. Don't loose precious days designing, coding the landing page and testing . </p>
+                    <a href="#" class="inline-flex items-center mt-4 font-semibold text-blue-600 lg:mb-0 hover:text-black " title="read more"> Read More » </a>
+                  </div>
+                </div>
+                <div class="w-full p-1 mx-auto lg:w-1/4">
+                  <div class="p-4 lg:p-2 rounded-xl bg-blueGray-50">
+                    <img class="object-cover object-center w-full mb-8 lg:h-48 md:h-36 rounded-xl" src="https://dummyimage.com/720x400/F3F4F7/8693ac" alt="blog">
+                    <h1 class="mx-auto mb-8 text-2xl font-semibold leading-none tracking-tighter text-black title-font"> Short length headline. </h1>
+                    <p class="mx-auto text-base font-medium leading-relaxed text-blueGray-700 ">You're about to launch soon and must be 100% focused on your product. Don't loose precious days designing, coding the landing page and testing . </p>
+                    <a href="#" class="inline-flex items-center mt-4 font-semibold text-blue-600 lg:mb-0 hover:text-black " title="read more"> Read More » </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+
+
+    <div class="caSrd w-1/4">
+      <div class="bg-indigo-300 p-7">
+        <img
+          src="https://spacejob.co.jp/wp-content/uploads/2021/03/revolt-164_6wVEHfI-unsplash-1024x683.jpg"
+          class=" rounded-btn object-cover h-60 w-72"
+        />
+        <div class="content flex mt-4"><p class="w-8/12"> Vestibulum tincidunt tellus a metus hendrer </p><p class=" w-3/6 items-start text-right font-bold text-lg" style="direction: rtl;"> 199.23 $ </p></div>
+        <div class="justify-center card-actions"><button class="btn btn-sm btn-accent normal-case  btn-success   mx-3">Add to Cart</button></div>
+      </div>
+    </div>
+
+
+  <footer-component></footer-component>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MenuComponent from "./components/menu.vue";
+import FooterComponent from "./components/footer.vue";
 
 export default {
-  name: 'App',
+  name: "",
   components: {
-    HelloWorld
-  }
-}
+    MenuComponent,
+    FooterComponent,
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/menu.vue
@@ -0,0 +1,83 @@
+<template> 
+
+<div>
+
+<div class="rounded-lg shadow bg-base-200 drawer">
+  <input id="my-drawer-3" type="checkbox" class="drawer-toggle"> 
+  <div class="flex flex-col drawer-content">
+    <div class="w-full navbar bg-base-300">
+      <div class="flex-none lg:hidden">
+        <label for="my-drawer-3" class="btn btn-square btn-ghost">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+          </svg>
+        </label>
+      </div> 
+      <div class="flex-1 px-2 mx-2">
+        <span>
+              Change screen size to show/hide menu
+            </span>
+      </div> 
+      <div class="flex-none hidden lg:block">
+        <ul class="menu horizontal uppercase">
+          <li>
+            <a class="rounded-btn">Cups</a>
+          </li> 
+          <li>
+            <a class="rounded-btn">Spoons</a>
+          </li>
+          <li>
+            <a class="rounded-btn">Lids</a>
+          </li>
+          <li>
+            <a class="rounded-btn">Accessories</a>
+          </li>
+          <li>
+            <a class="rounded-btn">Marketing</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div> 
+  <div class="drawer-side">
+    <label for="my-drawer-3" class="drawer-overlay"></label> 
+    <ul class="p-4 overflow-y-auto menu w-80 bg-base-100">
+      <li>
+        <a>Item 1</a>
+      </li> 
+      <li>
+        <a>Item 2</a>
+      </li>
+    </ul>
+  </div>
+</div>
+
+</div>
+  
+</template>
+<script>
+export default {
+  data() {
+    return {
+      isMenuOpen: false,
+       atTopOfPage: true
+    };
+  },
+beforeMount () {
+    window.addEventListener('scroll', this.handleScroll);
+},
+methods: {
+    // the function to call when the user scrolls, added as a method
+    handleScroll(){
+        // when the user scrolls, check the pageYOffset
+        if(window.pageYOffset>0){
+            // user is scrolled
+            if(this.view.atTopOfPage) this.view.atTopOfPage = false
+        }else{
+            // user is at top of page
+            if(!this.view.atTopOfPage) this.view.atTopOfPage = true
+        }
+    }
+}
+};
+</script>
